16113c24213f7848dfbe4bad56232246ad1481d4
Juan Brundi Sarmento Madeira <juanbrundism@gmail.com>
2021-12-13 23:31:27 +0700

    todolist.js/Create reducers for todolist

---
+++ src/store/reducers/todoList.js
@@ -0,0 +1,133 @@
+import {
+  PENDING,
+  FULFILLED,
+  FAILED,
+
+  GET_TODO_LIST,
+  CREATE_NEW_LIST,
+  REMOVE_TODO_LIST,
+  GET_LIST_BY_ID
+} from "../../constant/actions";
+
+const initialState = {
+  loading: false,
+  error: false,
+  data: [],
+  finishedList: [],
+  unfinishedList: [],
+  editedData: []
+}
+
+const divideListByStatus = (dataList = []) => {
+  const finishedList = []
+  const unfinishedList = []
+
+  dataList?.forEach(list => {
+    if (list?.status === 1) {
+      finishedList.push(list)
+    } else if (list?.status === 0) {
+      unfinishedList.push(list)
+    }
+  })
+
+  return {
+    finishedList,
+    unfinishedList
+  }
+}
+
+export default function todoList(state = initialState, action) {
+  switch (action.type) {
+    //FETCH ALL TODO LIST
+    case `${PENDING}${GET_TODO_LIST}`:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+        finishedList: [],
+        unfinishedList: [],
+        data: [],
+        editedData: []
+      }
+    case `${FULFILLED}${GET_TODO_LIST}`: {
+      const {
+        finishedList,
+        unfinishedList
+      } = divideListByStatus(action.payload);
+
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        data: action.payload,
+        finishedList,
+        unfinishedList,
+        editedData: []
+      }
+    }
+    case `${FAILED}${GET_TODO_LIST}`:
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+        finishedList: [],
+        unfinishedList: [],
+        data: [],
+        editedData: []
+      }
+
+    //CREATE NEW TODO LIST
+    case `${FULFILLED}${CREATE_NEW_LIST}`: {
+      const newData = [...state.data, action.payload]
+      const {
+        finishedList,
+        unfinishedList
+      } = divideListByStatus(newData);
+
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        data: newData,
+        finishedList,
+        unfinishedList,
+        editedData: []
+      }
+    }
+
+    //GET ONE TODO
+    case `${FULFILLED}${GET_LIST_BY_ID}`: {
+      const editedData = state.data?.filter(list => list.id === action.payload)
+
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        editedData: editedData || []
+      }
+    }
+
+    //REMOVE ONE TODO
+    case `${FULFILLED}${REMOVE_TODO_LIST}`: {
+      const newData = state.data?.filter(list => list.id !== action.payload)
+
+      const {
+        finishedList,
+        unfinishedList
+      } = divideListByStatus(newData);
+
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        data: newData,
+        finishedList,
+        unfinishedList,
+        editedData: []
+      }
+    }
+
+    default:
+      return state;
+  }
+}
