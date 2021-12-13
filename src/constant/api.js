a3c83306522b25725ffa1f8ab4408de66607b393
Juan Brundi Sarmento Madeira <juanbrundism@gmail.com>
2021-12-13 23:32:40 +0700

    Create constant variable

---
+++ src/constant/api.js
@@ -0,0 +1,3 @@
+const BASE_API = "https://virtserver.swaggerhub.com/hanabyan/todo/1.0.0/to-do-list";
+
+export { BASE_API };
---
+++ src/constant/index.js
@@ -0,0 +1,26 @@
+import { BASE_API } from "./api";
+import {
+  PENDING,
+  FULFILLED,
+  FAILED,
+  GET_TODO_LIST,
+  CREATE_NEW_LIST,
+  REMOVE_TODO_LIST,
+  GET_LIST_BY_ID
+} from "./actions";
+
+export {
+  //APIS
+  BASE_API,
+  
+  //TYPE STATUS
+  PENDING,
+  FULFILLED,
+  FAILED,
+
+  //ACTIONS
+  GET_TODO_LIST,
+  CREATE_NEW_LIST,
+  REMOVE_TODO_LIST,
+  GET_LIST_BY_ID
+};
